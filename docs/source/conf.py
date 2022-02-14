1358e324cc02a5201c051b731fcaf4c046795d87
lolxnn <inkyincolto@gmail.com>
2022-02-14 09:47:36 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'COP-docs'
+copyright = '2021, NEOS'
+author = ''
 
 release = '0.1'
 version = '0.1.0'
